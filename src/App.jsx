c8d770a96f7e8be1223a3345d736f883c9db311b
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2023-11-17 20:35:39 +0300

    add readme file

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { useSDK } from "@metamask/sdk-react";
 import SendPassword from "./components/SendForm";
 import List from "./components/ListSecrets";
 import { ethers } from "ethers";
+import Landing from "./components/Landing";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   const [account, setAccount] = useState();
-  const { sdk, connected, connecting, provider, chainId } = useSDK();
+  const { sdk, connected } = useSDK();
 
   const connect = async () => {
     try {
@@ -21,6 +23,66 @@ function App() {
   useEffect(() => {
     const initializeProvider = async () => {
       if (window.ethereum) {
+        console.log(account);
+        console.log(window.ethereum);
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        console.log(provider);
+        const network = await provider.getNetwork();
+        console.log(network.name);
+      }
+    };
+    initializeProvider();
+  }, []);
+
+  const handleOnMove = (e) => {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-0x";
+
+    const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
+      randomString = (length) => Array.from(Array(length)).map(randomChar).join("");
+
+    const card = document.querySelector(".card"),
+      letters = card.querySelector(".card-letters");
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    letters.style.setProperty("--x", `${x}px`);
+    letters.style.setProperty("--y", `${y}px`);
+
+    letters.innerText = randomString(1500);
+  };
+
+  const [see, setSee] = useState(true);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+function Dashboard() {
+  const [account, setAccount] = useState();
+  const { sdk, connected } = useSDK();
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn(`failed to connect..`, err);
+    }
+  };
+
+  useEffect(() => {
+    const initializeProvider = async () => {
+      if (window.ethereum) {
+        console.log(account);
         console.log(window.ethereum);
         const provider = new ethers.BrowserProvider(window.ethereum);
         console.log(provider);
